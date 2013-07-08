a627f99d27af93fc790b7ade0dc1539a63334a5e
Paul Minogue <johnpaulminogue@gmail.com>
2013-07-08 22:26:20 +0100

    Updated web.js to read from file

--- web.js
+++ web.js
@@ -1,10 +1,32 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var fs = require("fs");
+var fileName = "index.html";
+fs.exists(fileName, function(exists) {
+  if (exists) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+        var buffer = new Buffer(stats.size);
+        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+          var data = buffer.toString("utf8", 0, buffer.length);
+          console.log(data);
+          fs.close(fd);
+        });
+      });
+    });
+  }
+});
+
+var data = fs.readFileSync("index.html", "utf8");
 app.get('/', function(request, response) {
-var fs = require('fs');
-var fileName = fs.readFileSync("index.html").toString();
-var buf = new Buffer(fileName);
-for (var i = 0; i < fileName.length; i++) {
-	buf[i] = str.charCodeA(i);
-	}
-  response.send(buf);
+
+  response.send(data);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);//
 });
 
