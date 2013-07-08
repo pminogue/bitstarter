af2a365142e00e4d016f7876dc82bb7bc0a87826
Paul Minogue <johnpaulminogue@gmail.com>
2013-07-08 22:10:41 +0100

    Updated web.js to read from file

--- web.js
+++ web.js
@@ -1,13 +1,10 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
 app.get('/', function(request, response) {
-	response.send('Hello World 2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-	console.log("Listening on " + port);
+var fs = require('fs');
+var fileName = fs.readFileSync("index.html").toString();
+var buf = new Buffer(fileName);
+for (var i = 0; i < fileName.length; i++) {
+	buf[i] = str.charCodeA(i);
+	}
+  response.send(buf);
 });
 
