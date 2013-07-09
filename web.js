38828dbe144fabfb14e622b437c7741785c2a2a5
Paul Minogue <johnpaulminogue@gmail.com>
2013-07-09 20:38:30 +0100

    Updated web.js to read from file

--- web.js
+++ web.js
@@ -19,8 +19,8 @@ app.get('/', function(request, response) {
     response.send(data);
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//    console.log("Listening on " + port);
+//});
 
