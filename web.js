8eafdb453e3c8498e9fe8c2b9150b227f23f0c1c
Paul Minogue <johnpaulminogue@gmail.com>
2013-07-15 22:45:38 +0100

    Updated index.html and web.js for hw4

--- web.js
+++ web.js
@@ -1,25 +1,15 @@
 var express = require('express');
-var app = express.createServer(express.logger());
 var fs = require("fs");
-var file = "index.html";
+var htmlfile = "index.html";
 
-fs.stat(file, function(error, stats) {
-	fs.open(file, "r", function(error, fd) {
-		var buffer = new Buffer(stats.size);
-		fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
-			var data = buffer.toString("utf8", 0, buffer.length);
-			console.log(data);
-			fs.close(fd);
-		});
-	});
-});
+var app = express.createServer(express.logger());
 
-var data = fs.readFileSync(file, "utf8");
 app.get('/', function(request, response) {
-    response.send(data);
+	var html = fs.readFileSync(htmlfile).toString();
+	respnse.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
